74072325e83548aea04833e07d996f996d60ce26
jeovane lima <145929373+JeovaneSilva@users.noreply.github.com>
2024-06-08 21:39:13 -0300

    criando a conexao com api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,66 @@
 'use client'
 
-import { FaSearch, FaSun,FaWind } from "react-icons/fa";
+import { FaDroplet,FaWind, FaMagnifyingGlass,FaTemperatureArrowUp  } from "react-icons/fa6";
 import { useState } from "react";
-import Card from "@/components/CardDados";
-import { log } from "console";
-
-const teste = <FaWind />
-const test2 = process.env.NEXT_PUBLIC_ANALYTICS_ID
-
-console.log(test2);
-
+import Card from "@/components/CardDados"
+import { getWeather } from "@/services/api";
 
 export default function Home() {
 
   const [city,Setcity] = useState("")
-  console.log(city);
-  
+  const [nameCity,SetNameCity] = useState("")
+  const [temperatura,SetTemperatura] = useState(0)
+  const [tempMax,SetTemMax] = useState(0)
+  const [tempMin,SetTemMin] = useState(0)
+  const [descricao,SetDescricao] = useState(" ")
+  const [humidade,setHumidade] = useState("")
+  const [velocidade,setVelocidade] = useState("")
+  const [srcIcon,setSrcIcon] = useState("")
 
+
+  const showWheater = async (city:string) => {
+    const data = await getWeather(city)
+    const temp = parseInt(data.main.temp)
+
+    SetNameCity(data.name)
+    SetTemperatura(temp)
+    SetTemMax(200)
+    SetDescricao(data.weather[0].description)
+    setSrcIcon(`https://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
+    setHumidade(`${data.main.humidity}%`)
+    setVelocidade(`${data.wind.speed}Km/h`)
+  }
+
+  const searchCity = () =>{
+    showWheater(city)
+  }
+  
   return (
     <main className="w-full h-full flex flex-col justify-center items-center">
       <header className="w-full flex flex-col items-center justify-center">
         <h1 className="text-3xl mt-[1rem]">Clima</h1>
-        <div className="mt-[1rem] flex ">
+        <div className="mt-[1rem] flex justify-center items-center ">
           <input className="w-[200px] h-[35px] border-blue-500 border-2 rounded-[20px] outline-none pl-[10px]" type="search" name="namecity"
 
             onChange={(e) =>{Setcity(e.target.value)}}
           />
-          <button className="ml-[1rem] text-2xl"><FaSearch /></button>
+          <button className="ml-[1rem] text-2xl bg-blue-500 p-2 rounded-[8px]" onClick={searchCity}><FaMagnifyingGlass /></button>
         </div>
       </header>
 
       <section className="mt-[2rem] flex flex-col justify-center items-center">
-        <h2 className="text-xl">Cascavel</h2>
+        <h2 className="text-xl">{nameCity}</h2>
         <div className="flex flex-col w-full justify-center items-center mt-6">
-          <FaSun className="text-yellow-400 text-5xl" />
-          <span className="">29°C</span>
+          <span className="">{temperatura+" °C"}</span>
+          <img src={srcIcon} alt="" />
+          <p>{descricao}</p>
         </div>
 
         <div className="w-[80vw] grid grid-cols-2 place-items-center mt-8">
-            <Card icon={teste} content={"Nublado"} />
-            <Card icon={teste} content={"Max - Min"} />
-            <Card icon={teste} content={20} />
-            <Card icon={teste} content={"humidade"} />
+            <Card icon={<FaTemperatureArrowUp />} content={"max"} />
+            <Card icon={<FaWind />} content={"Min"} />
+            <Card icon={<FaWind />} content={velocidade} />
+            <Card icon={<FaDroplet />} content={humidade} />
         </div>
       </section>
     </main>
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,1 +1,13 @@
 // 148246e153ee8aeaab6fadbaa0c8ef0c
+const apiUrl = process.env.NEXT_PUBLIC_URL_API
+const apikey = process.env.NEXT_PUBLIC_API_KEY
+
+export const getWeather = async(city:string) =>{
+    const apiWeatherUrl = `${apiUrl}q=${city}&units=metric&appid=${apikey}&lang=pt_br`
+
+    const res = await fetch(apiWeatherUrl)
+    const data = await res.json()
+    console.log(data)
+
+    return data
+}
